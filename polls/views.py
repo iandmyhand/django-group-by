0cdd4434a10f21195ba8019e65ae21098d2d50db
SeomGi, Han <iandmyhand@gmail.com>
2017-08-06 20:24:22 +0900

    Add a temporary view of Polls app and connect it in urls.py.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return JsonResponse({'foo': 'bar'})
