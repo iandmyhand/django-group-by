b3d034f0ccfb516c574f70b4b81119db20c7c9bb
SeomGi, Han <iandmyhand@gmail.com>
2017-08-09 20:14:16 +0900

    Add an update_date field to Question model.

--- polls/models.py
+++ polls/models.py
@@ -5,4 +5,5 @@ class Question(models.Model):
     question_category = models.CharField(max_length=45)
     question_text = models.CharField(max_length=200)
     point = models.IntegerField(default=0)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    update_date = models.DateTimeField('date updated', auto_now=True)
